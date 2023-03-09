880f2e25f94e403567d8c682ec9751a709ee537d
Cheng Yao <kukat@me.com>
2023-03-09 23:57:37 +0800

    More neotest actions

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,8 +3,11 @@ return {
     "nvim-neotest/neotest",
     keys = {
       { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "File" },
-      { "<leader>to", "<cmd>lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
+      { "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Last" },
+      { "<leader>tn", "<cmd>lua require('neotest').run.run()<cr>", desc = "Nearest" },
+      { "<leader>tw", "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>", desc = "Watch" },
       { "<leader>tS", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Summary" },
+      { "<leader>to", "<cmd>lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
