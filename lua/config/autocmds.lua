f32727ba7d3ed4a1e5c829328788a3613c3016f4
Cheng Yao <cyao@thoughtworks.com>
2024-01-02 11:38:40 +0800

    ruby & terraform support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.podspec", "Podfile", "Fastfile" },
+  pattern = { "*.podspec", "Podfile", "Fastfile", "Appfile" },
   command = "set filetype=ruby",
 })
